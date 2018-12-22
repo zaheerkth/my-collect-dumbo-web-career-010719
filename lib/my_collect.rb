46e89587bafda6fb1db031bf6bee96460b92cdc3
zaheer <m.abbaszaheer@gmail.com>
2018-12-22 00:07:57 -0500

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
-
-
+def my_collect(collection)# put argument(s) here
+  	if block_given?
+    	new_collection=[]
+    	count = 0
+    	while count < collection.size
+     	new_collection << yield(collection[count])
+     	count+=1
+    	end
+    	new_collection
+	else
+		puts "Hey! No block was given!"
+  end
+ 
+end
